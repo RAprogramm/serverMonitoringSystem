d369df1b33957d8dc46bf64edafff08515ff3dd6
RAprogramm <andrey.rozanov.vl@gmail.com>
2023-11-13 09:47:47 +0900

    add: Pusher

--- go.mod
+++ go.mod
@@ -8,7 +8,10 @@ require (
 	github.com/CloudyKit/fastprinter v0.0.0-20200109182630-33d98a066a53 // indirect
 	github.com/PuerkitoBio/goquery v1.8.1 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/dimiro1/ipe v1.2.0 // indirect
+	github.com/golang/glog v1.1.2 // indirect
 	github.com/gorilla/css v1.0.1 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
@@ -19,7 +22,8 @@ require (
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/ssor/bom v0.0.0-20170718123548-6386211fdfcf // indirect
 	github.com/toorop/go-dkim v0.0.0-20201103131630-e1cd1a0a5208 // indirect
-	golang.org/x/net v0.10.0 // indirect
+	goji.io v2.0.2+incompatible // indirect
+	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 )
 
--- internal/handlers/pusher.go
+++ internal/handlers/pusher.go
@@ -1,9 +1,44 @@
+// Package handlers provides HTTP handlers for various functionalities.
 package handlers
 
-import "net/http"
+import (
+	"io"
+	"log"
+	"net/http"
+	"strconv"
 
-// PusherAuth method ...
+	"github.com/pusher/pusher-http-go"
+)
+
+// PusherAuth method handles authentication for Pusher presence channels.
 func (repo *DBRepo) PusherAuth(w http.ResponseWriter, r *http.Request) {
-    userID := repo.App.Session.GetInt()
-}
+	// Get the user ID from the session
+	userID := repo.App.Session.GetInt(r.Context(), "userID")
 
+	// Get user information from the database
+	u, _ := repo.DB.GetUserById(userID)
+
+	// Read the request body
+	params, _ := io.ReadAll(r.Body)
+
+	// Create presence data for the Pusher member
+	presenceData := pusher.MemberData{
+		UserID: strconv.Itoa(userID),
+		UserInfo: map[string]string{
+			"name": u.FirstName,
+			"id":   strconv.Itoa(userID),
+		},
+	}
+
+	// Authenticate presence channel with Pusher
+	response, err := app.WsClient.AuthenticatePresenceChannel(params, presenceData)
+	if err != nil {
+		log.Println("Error in response")
+		log.Println(err)
+		return
+	}
+
+	// Set the response header and write the response
+	w.Header().Set("Content-Type", "application/json")
+	_, _ = w.Write(response)
+}
